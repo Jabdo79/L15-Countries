9910090cca137dac53c37e53530d920d6d017e5e
Jonathan Abdo <jabdo79@gmail.com>
2016-06-16 11:52:26 -0400

    finish

--- src/CountriesApp.java
+++ src/CountriesApp.java
@@ -1,37 +1,44 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CountriesApp {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to the Countries Maintenance Application!");
-		
+
+		CountriesTextFile.createCountriesFile();
+
 		String country = "";
 		boolean cont = true;
-		while(cont){
-			System.out.println("\n1.See the list of countries\n2.Add a country\n3.Exit\n");
-			int menuChoice = InputCheck.getInt(sc, "Enter a menu number: ", 1, 3);
-			
-			switch(menuChoice){
-			//see list of countries
+		while (cont) {
+			System.out.println("\n1.See the list of countries\n2.Add a country\n3.Clear List\n4.Exit\n");
+			int menuChoice = InputCheck.getInt(sc, "Enter a menu number: ", 1, 4);
+
+			System.out.println("");
+			switch (menuChoice) {
+			// see list of countries
 			case 1:
-				System.out.println("");
 				CountriesTextFile.listCountries();
 				break;
-			//add country
+			// add country
 			case 2:
 				System.out.print("Enter a country: ");
 				country = sc.nextLine();
 				CountriesTextFile.addCountry(country);
 				break;
-			//exit
+			// clear list
 			case 3:
-				System.out.println("Buh-bye!");
-				cont=false;
+				CountriesTextFile.clearCountries();
 				break;
-			}	
+			// exit
+			case 4:
+				System.out.println("Buh-bye!");
+				cont = false;
+				break;
+			}
 		}
 	}
 }
--- src/CountriesTextFile.java
+++ src/CountriesTextFile.java
@@ -1,12 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CountriesTextFile {
-	
-	public static void listCountries(){
-		
+
+	private static File file;
+	private static final Charset charset = Charset.forName("US-ASCII");
+
+	public static void createCountriesFile() throws IOException {
+		// creates a path including filename
+		Path p = Paths.get("src/countries.txt");
+		// creates a new file using the path
+		file = new File(p.toString());
+		// if file does not exist, it will create it
+		file.createNewFile();
+	}
+
+	public static void listCountries() throws IOException {
+		// create new reader using the file's path and US char set
+		BufferedReader br = Files.newBufferedReader(file.toPath(), charset);
+		String line = null;
+		boolean empty = true;
+		// while the line is not empty, print it
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+			empty = false;
+		}
+		if(empty)
+			System.out.println("List is empty!");
+		br.close();
+	}
+
+	public static void addCountry(String country) throws IOException {
+		// opens file with intention of appending
+		FileWriter fw = new FileWriter(file.getAbsolutePath(), true); 
+		BufferedWriter bw = new BufferedWriter(fw);
+		// write to file
+		bw.write(country+"\n");
+		bw.close();
+		System.out.println(country + " has been saved!");
 	}
 	
-	public static void addCountry(String country){
-		
-		System.out.println(country + " has been saved!");
+	public static void clearCountries() throws IOException{
+		//opens/clears the file
+		FileWriter fw = new FileWriter(file.getAbsolutePath()); 
+		fw.close();
+		System.out.println("List has been cleared!");
 	}
 }
